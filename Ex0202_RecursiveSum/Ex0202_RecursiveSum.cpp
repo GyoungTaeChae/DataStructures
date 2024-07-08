13e5cdfd9f8764f074b3096b74fa7ae67d874bcc
GyoungTaeChae <foolips@naver.com>
2024-07-08 11:53:54 +0900

    commit

--- Ex0202_RecursiveSum/Ex0202_RecursiveSum.cpp
+++ Ex0202_RecursiveSum/Ex0202_RecursiveSum.cpp
@@ -16,14 +16,17 @@ int Sum(int* arr, int n)
 
 int RecurSum(int* arr, int n)
 {
-	/* 힌트: 역순으로 더하기 (매개변수에 시작 인덱스를 추가하지 않아도 되기 때문)
-	  (1 + 2 + ... 8 + 9 + 10)
-	= (1 + 2 + ... 8 + 9) + 10
-	= ((1 + 2 + ... 8) + 9) + 10
-	= ...
-	*/
+	
+	if (n <= 0)
+{
+    return 0;
+}
+else
+{	
+	
+    return RecurSum(arr,n-1)+arr[n-1];
+}
 
-	return 0; // <- TODO: 
 }
 
 int main()
@@ -32,8 +35,8 @@ int main()
 
 	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	int n = sizeof(arr) / sizeof(arr[0]);
-
-	cout << Sum(arr, n) << endl;
+	
+	// cout << Sum(arr, n) << endl;
 	cout << RecurSum(arr, n) << endl;
 
 	return 0;
