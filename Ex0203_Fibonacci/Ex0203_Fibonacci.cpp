91cc7474540024770c561b9b384f6e96c0a32afc
GyoungTaeChae <foolips@naver.com>
2024-07-09 11:13:44 +0900

    commit

--- Ex0203_Fibonacci/Ex0203_Fibonacci.cpp
+++ Ex0203_Fibonacci/Ex0203_Fibonacci.cpp
@@ -13,19 +13,12 @@ int Fibonacci(int n)
 		return 1; // F1 = 1
 	else
 	{
-		int prev1 = 0;
-		int prev2 = 1;
 		int fn = 0;
-		for (int i = 2; i<=n; i++){
-			fn = prev1+prev2;
-
-			prev1 = prev2;
-			prev2 = fn;
-
-		}
-
+		//TODO
 		return fn;
 	}
+
+	
 }
 
 int RecurFibonacci(int n)
