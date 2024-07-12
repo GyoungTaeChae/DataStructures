94057269878a489d549271eb7eab0ed66f59afe9
GyoungTaeChae <foolips@naver.com>
2024-07-12 11:00:27 +0900

    commit

---
+++ Ex0302_Polynomial/build/CMakeFiles/PolynomialExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../PolynomialExecutable"
+  "../PolynomialExecutable.pdb"
+  "CMakeFiles/PolynomialExecutable.dir/Ex0302_Polynomial.cpp.o"
+  "CMakeFiles/PolynomialExecutable.dir/Polynomial.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PolynomialExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
