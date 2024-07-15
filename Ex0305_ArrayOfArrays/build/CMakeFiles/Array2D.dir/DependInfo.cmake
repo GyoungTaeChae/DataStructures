c9dd382f52db8d8d68b4b9351bf273544a634ddc
GyoungTaeChae <foolips@naver.com>
2024-07-15 11:16:41 +0900

    commit

---
+++ Ex0305_ArrayOfArrays/build/CMakeFiles/Array2D.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cgt20/Codingtest/DataStructures/Ex0305_ArrayOfArrays/Array2D.cpp" "/home/cgt20/Codingtest/DataStructures/Ex0305_ArrayOfArrays/build/CMakeFiles/Array2D.dir/Array2D.cpp.o"
+  "/home/cgt20/Codingtest/DataStructures/Ex0305_ArrayOfArrays/Ex0305_ArrayOfArrays.cpp" "/home/cgt20/Codingtest/DataStructures/Ex0305_ArrayOfArrays/build/CMakeFiles/Array2D.dir/Ex0305_ArrayOfArrays.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Ex0305_ArrayOfArrays/build/CMakeFiles/Array2D.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../Array2D"
+  "../Array2D.pdb"
+  "CMakeFiles/Array2D.dir/Array2D.cpp.o"
+  "CMakeFiles/Array2D.dir/Ex0305_ArrayOfArrays.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Array2D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
