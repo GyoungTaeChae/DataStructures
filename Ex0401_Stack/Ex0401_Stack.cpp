bbb04465e28e0ba1d8bf1b96a085243d326aca62
GyoungTaeChae <foolips@naver.com>
2024-07-17 10:55:49 +0900

    commit

--- Ex0401_Stack/Ex0401_Stack.cpp
+++ Ex0401_Stack/Ex0401_Stack.cpp
@@ -9,38 +9,38 @@ int main()
 	// 일종의 가방(bag), 컨테이너(container)
 	// LIFO(Last-In-First-Out)
 
-	Stack<char> s;
+	// Stack<char> s;
 
-	s.Push('A');
-	s.Print();
+	// s.Push('A');
+	// s.Print();
 
-	s.Push('B');
-	s.Print();
+	// s.Push('B');
+	// s.Print();
 
-	s.Push('C');
-	s.Print();
+	// s.Push('C');
+	// s.Print();
 
-	cout << "Top = " << s.Top() << endl;
+	// cout << "Top = " << s.Top() << endl;
 
-	s.Pop();
-	s.Print();
+	// s.Pop();
+	// s.Print();
 
-	cout << "Top = " << s.Top() << endl;
+	// cout << "Top = " << s.Top() << endl;
 
-	s.Pop();
-	s.Print();
+	// s.Pop();
+	// s.Print();
 
-	s.Push('E');
-	s.Print();
+	// s.Push('E');
+	// s.Print();
 
-	s.Pop();
-	s.Print();
+	// s.Pop();
+	// s.Print();
 
-	cout << "Top = " << s.Top() << endl;
+	// cout << "Top = " << s.Top() << endl;
 
-	s.Pop();
+	// s.Pop();
 
-	cout << "Top = " << s.Top() << endl; // 비었을 때 Top() 불가
+	// cout << "Top = " << s.Top() << endl; // 비었을 때 Top() 불가
 
 	// 다른 자료형의 stack도 쉽게 만들 수 있어요.
 	// {
@@ -52,16 +52,28 @@ int main()
 
 	// 문자열 순서 뒤집어서 출력하기 (스택 사용하기 연습)
 	{
-		//const char str[] = "Hello, World!";
-		//const int size = sizeof(str) - 1; // 마지막 '\0' 제외
 
-		//cout << "Input: " << str << endl;
+		const char str[] = "Hello, World!";
+		const int size = sizeof(str) - 1; // 마지막 '\0' 제외
 
-		//Stack<char> stack;
+		cout << "Input: " << str << endl;
 
-		//// TODO:
+		Stack<char> stack;
+		
+		for (auto i = 0; i<size; i++)
+		{
+			stack.Push(str[i]);
+		}	
+		std::cout << "Output:";
+		for (auto i = size; i>0; i--)
+		{
+			std::cout << stack.Top();
+			stack.Pop();
+		}
+		
+		// TODO:
 
-		//cout << endl;
+		std::cout << endl;
 	}
 
 	return 0;
---
+++ Ex0402_Maze/build/CMakeFiles/Ex0402_Maze.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../Ex0402_Maze"
+  "../Ex0402_Maze.pdb"
+  "CMakeFiles/Ex0402_Maze.dir/Ex0402_Maze.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ex0402_Maze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
