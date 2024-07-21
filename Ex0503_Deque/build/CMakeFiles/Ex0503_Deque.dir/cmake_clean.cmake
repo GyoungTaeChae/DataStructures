6fc23df5413260497d3eb3e7eabb1f7df225f283
GyoungTaeChae <foolips@naver.com>
2024-07-21 17:42:36 +0900

    commit

---
+++ Ex0503_Deque/build/CMakeFiles/Ex0503_Deque.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../Ex0503_Deque"
+  "../Ex0503_Deque.pdb"
+  "CMakeFiles/Ex0503_Deque.dir/Ex0503_Deque.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ex0503_Deque.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- shared/Deque.h
+++ shared/Deque.h
@@ -34,6 +34,8 @@ public:
 		if (Base::IsFull())
 			Base::Resize();
 
+		Base::queue_[Base::front_] = item;
+		Base::front_ = (Base::front_ -1 +Base::capacity_) % Base::capacity_;
 		// TODO:
 	}
 
@@ -50,10 +52,17 @@ public:
 	void PopBack()
 	{
 		assert(!Base::IsEmpty());
-
+		// Base::rear_ = (Base::rear_+1)%Base::capacity_;
+		// Base::queue_[Base::rear_] = item;
 		// TODO:
+		Base::rear_ = (Base::rear_ -1 +Base::capacity_) % Base::capacity_;
 	}
 
 private:
 	// Queue와 동일
+	// T* queue_; // array for queue elements
+	// int front_ = 0; // 시작 인덱스보다 하나 작은 값
+	// int rear_ = 0; // 마지막 인덱스 (첫 값은 1에 추가)
+	// int capacity_; // 빈 칸을 하나 둬야 하기 때문에 필요 메모리는 최대 저장량 + 1
+	// bool print_debug_ = false;
 };
